ee98a43d2965295e3cda1dd3ab2b003aa5feb40b
Mahesh <thakkilm@mail.gvsu.edu>
2023-11-02 11:18:57 -0400

    Final Changes

---
+++ src/main/java/com/Mahesh/rest/webservices/restfulwebservices/jwt/JwtTokenResponse.java
@@ -0,0 +1,5 @@
+package com.Mahesh.rest.webservices.restfulwebservices.jwt;
+
+public record JwtTokenResponse(String token) {}
+
+
---
+++ target/classes/data.sql
@@ -0,0 +1,9 @@
+insert into todo(id,description,done,target_date,username)
+values (1001,'Learn Spring',false,CURRENT_DATE(),'Mahesh');
+
+insert into todo(id,description,done,target_date,username)
+values (1002,'Learn Java Script',false,CURRENT_DATE(),'Mahesh');
+
+
+insert into todo(id,description,done,target_date,username)
+values (1003,'Learn Angular',false,CURRENT_DATE(),'Mahesh');
